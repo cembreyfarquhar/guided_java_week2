d832246b1b0641ab371287af1f466d394cfc2f84
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-08-06 15:04:55 -0500

    first commit

---
+++ src/main/java/com/lambdaschool/webemployeesreal/WebemployeesrealApplication.java
@@ -0,0 +1,14 @@
+package com.lambdaschool.webemployeesreal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebemployeesrealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebemployeesrealApplication.class, args);
+    }
+
+
+}
