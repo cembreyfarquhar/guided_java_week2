c4c1db994d38f346bdf2ac0de7c000d84652b97c
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-08-07 13:10:31 -0500

    adds controller for getting employee by id

--- src/main/java/com/lambdaschool/webemployeesreal/EmployeeController.java
+++ src/main/java/com/lambdaschool/webemployeesreal/EmployeeController.java
@@ -3,6 +3,7 @@ package com.lambdaschool.webemployeesreal;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,12 @@ public class EmployeeController {
     @GetMapping(value = "/allemployees")
     public ResponseEntity<?> getAllEmployees() {
         return new ResponseEntity<>(WebemployeesrealApplication.ourEmpList.empList, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/employee/{id}")
+    public ResponseEntity<?> getEmpDetail(@PathVariable long id) {
+        Employee rtnEmployee = WebemployeesrealApplication.ourEmpList.findEmployee(e -> (e.getId() == id));
+        return new ResponseEntity<>(rtnEmployee, HttpStatus.OK);
     }
 }
 
